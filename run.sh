3b710526a69a205e42beefb1984a8742ce6f3c42
James Eckersall <james.eckersall@gmail.com>
2019-06-30 08:29:11 +0100

    Added SITE_URL variable handling

--- run.sh
+++ run.sh
@@ -6,10 +6,15 @@ if [ ! -f $CONFIG_FILE ]; then
   cat <<EOF > $CONFIG_FILE
 [main]
 api_key = ${API_KEY}
+[misc]
+host_whitelist = ${SITE_URL}
 EOF
 else
   if [ -z "${API_KEY}" ]; then
     sed -i -e "s/^api_key = .*$/api_key = ${API_KEY}/" $CONFIG_FILE
+  fi
+  if [ -z "${SITE_URL}" ]; then
+    sed -i -e "s/^host_whitelist = .*$/host_whitelist = ${SITE_URL}/" $CONFIG_FILE
   fi
 fi
 
